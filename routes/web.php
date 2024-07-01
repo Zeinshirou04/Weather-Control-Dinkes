4bd2a621c523b9f316f5f6c12cf54b4741dd6678
Zen <Farras.FF6@gmail.com>
2024-07-01 17:41:50 +0700

    Commit Default Route

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Device\RegisterDeviceController;
 
 Route::post('devices/create', [RegisterDeviceController::class, 'create'])->name('device.create');
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+});
 
 require __DIR__.'/auth.php';
