a2d523dbe00a63f16721b0c192c9f327179884da
Zen <Farras.FF6@gmail.com>
2024-07-06 02:18:24 +0700

    Commit View Progress

--- app/Http/Controllers/Home/HomePageController.php
+++ app/Http/Controllers/Home/HomePageController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Devices\Device;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class HomePageController extends Controller
 {
     public function index() {
-        return Inertia::render('Home/WelcomePage');
+        $data = [
+            'latitude' => Device::where('id', '71f913f7-821b-3b2b-9494-79e2cd0a9856')
+            ->first()
+            ->toArray()['latitude'],
+            'longitude' => Device::where('id', '71f913f7-821b-3b2b-9494-79e2cd0a9856')
+            ->first()
+            ->toArray()['longitude'],
+        ];
+        return Inertia::render('Home/WelcomePage', $data);
     }
 }
--- resources/js/Pages/Home/WelcomePage.jsx
+++ resources/js/Pages/Home/WelcomePage.jsx
@@ -3,11 +3,11 @@ import { React, useState, useEffect } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import axios from "axios";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 import Home from "@/Layouts/HomeLayout";
 
-export default function Welcome() {
+export default function Welcome({ latitude, longitude }) {
     const responsive = {
         desktop: {
             breakpoint: {
@@ -18,34 +18,50 @@ export default function Welcome() {
         },
     };
 
-    axios
-        .get(
-            "https://newsdata.io/api/1/latest?apikey=pub_4786655fa8d619a9dadf8d21dced948677e52&country=id&q=cuaca"
-        )
-        .then((response) => {
-            console.log(response.data);
-        });
+    // axios
+    //     .get(
+    //         "https://newsdata.io/api/1/latest?apikey=pub_4786655fa8d619a9dadf8d21dced948677e52&country=id&q=cuaca"
+    //     )
+    //     .then((response) => {
+    //         console.log(response.data);
+    //     });
 
-    let [data, setData] = useState([]);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios
-            .get(
-                "https://weather.robotlintang.id/api/device/71f913f7-821b-3b2b-9494-79e2cd0a9856",
-                {
-                    // data: '71f913f7-821b-3b2b-9494-79e2cd0a9856'
-                    headers: {
-                        "User-Agent": "Axios",
-                    },
-                }
-            )
-            .then((response) => {
-                // console.log(response.data);
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "https://weather.robotlintang.id/api/device/71f913f7-821b-3b2b-9494-79e2cd0a9856",
+                    {
+                        headers: {
+                            "User-Agent": "Axios",
+                        },
+                    }
+                );
                 setData(response.data);
-            });
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
     }, []);
 
-    console.log(data);
+    if (loading) return <div className="w-svw h-svh flex flex-col justify-center items-center">
+        <img className="w-32" src="assets/img/logoDinkes.png" alt="Logo Kementrian Kesehatan" />
+        <div className="mt-8">
+            <p className="text-lg">Mohon tunggu...</p>
+        </div>
+    </div>;
+    // if (error) return <div>Error: {error.message}</div>;
+
+    // console.log(data);
+
     const carouselItems = [
         <div className="carousel-item active float-left w-full h-full">
             <img
@@ -67,16 +83,6 @@ export default function Welcome() {
         <Home>
             <Head title="Pantau Cuaca Kotamu!" />
 
-            <APIProvider apiKey={"AIzaSyA9zha9KW9svjEDpCcyEP0YpFBoRyU2H6E"}>
-                <Map
-                    style={{ width: "100vw", height: "100vh" }}
-                    defaultCenter={{ lat: 22.54992, lng: 0 }}
-                    defaultZoom={6}
-                    gestureHandling={"greedy"}
-                    disableDefaultUI={true}
-                />
-            </APIProvider>
-
             <div className="w-full h-52">
                 <Carousel
                     className="h-full rounded-lg"
@@ -92,22 +98,67 @@ export default function Welcome() {
             </div>
 
             <article className="w-full flex flex-row gap-2 h-full mt-12">
-                <section className="w-3/4 flex flex-col gap-8">
+                <section className="w-3/5 flex flex-col gap-8">
                     <div className="w-full h-40 bg-[#A7D7C5]/40 shadow-md rounded-lg flex flex-col py-2">
                         <div className="h-12 w-full border-b-gray-400 border-b-2 px-6">
-                            <h5 className="font-semibold text-gray-600">
-                                Cuaca dini hari di,{" "}
+                            <h5 className="font-semibold text-gray-900">
+                                Cuaca dini hari di, Miroto
                             </h5>
                         </div>
-                        <div className="h-full w-full"></div>
+                        <div className="h-full w-full flex flex-col gap-2 px-6 py-2">
+                            <div className="w-full flex flex-row items-center gap-2">
+                                <img
+                                    className="w-12"
+                                    src="assets/svg/cloud.svg"
+                                    alt="Cloud Icon"
+                                />
+                                <p className="text-lg">
+                                    Suhu berada pada {data.data["temperature"]}
+                                    <span>&deg;C</span>
+                                </p>
+                            </div>
+                            <div className="w-full flex flex-row items-center gap-2">
+                                <img
+                                    className="w-12"
+                                    src="assets/svg/air.svg"
+                                    alt="Cloud Icon"
+                                />
+                                <p className="text-lg">
+                                    Kecepatan angin berada pada {data.data["avg_wind_spd"]}
+                                    <span>m/s</span>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     <div className="w-full flex flex-row gap-8">
                         <div className="w-full h-40 bg-[#A7D7C5]/40 shadow-md rounded-lg"></div>
                         <div className="w-full h-40 bg-[#A7D7C5]/40 shadow-md rounded-lg"></div>
                     </div>
                 </section>
-                <aside className="w-1/4">
-                    <div className="w-full h-40 bg-[#A7D7C5]/40 shadow-md rounded-lg"></div>
+                <aside className="w-2/5 h-auto">
+                    <div className="w-full h-full bg-[#A7D7C5]/40 shadow-md rounded-lg px-2 py-2">
+                        <APIProvider
+                            apiKey={"AIzaSyA9zha9KW9svjEDpCcyEP0YpFBoRyU2H6E"}
+                        >
+                            <Map
+                                className="w-full h-full border-2 border-[#A7D7C5]"
+                                defaultCenter={{
+                                    lat: data.lat,
+                                    lng: data.long,
+                                }}
+                                defaultZoom={18}
+                                gestureHandling={"greedy"}
+                                // disableDefaultUI={true}
+                            >
+                                <Marker
+                                    position={{
+                                        lat: data.lat,
+                                        lng: data.long,
+                                    }}
+                                />
+                            </Map>
+                        </APIProvider>
+                    </div>
                 </aside>
             </article>
 
